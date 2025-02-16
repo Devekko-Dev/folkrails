a1f9e9a1308a696e1b6884a561d759181e579e68
Nicholas Roberts <niccolox@devekko.com>
2025-02-16 14:47:27 -0800

    cart

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,6 @@
 class Product < ApplicationRecord
+  has_many :line_items
+  before_destroy :ensure_not_referenced_by_any_line_item
   has_one_attached :image do |attachable|
     attachable.variant :thumb, resize_to_limit: [100, 100]
   end
@@ -13,5 +15,15 @@ class Product < ApplicationRecord
       errors.add(:image, "must be a GIF, JPG, PNG or WEBP image")
     end
   end
+
+  private
+  # ensure that there are no line items referencing this product
+  def ensure_not_referenced_by_any_line_item
+    unless line_items.empty?
+      errors.add(:base, "Line Items present")
+      throw :abort
+    end
+  end
+
   validates :price, numericality: { greater_than_or_equal_to: 0.01 }
 end
