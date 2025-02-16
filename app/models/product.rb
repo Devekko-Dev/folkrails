70a6b900230e986a16cce89b14afd7549f882314
Nicholas Roberts <niccolox@devekko.com>
2025-02-16 13:01:53 -0800

    product validation reorder

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,11 +1,9 @@
 class Product < ApplicationRecord
-  validates :title, :subtitle, :description, :image, :price, presence: true
-  validates :price, numericality: { greater_than_or_equal_to: 0.01 }
-  validates :title, uniqueness: true  
   has_one_attached :image
   after_commit -> { broadcast_refresh_later_to "products" }
+  validates :title, :subtitle, :description, :image, :price, presence: true
+  validates :title, uniqueness: true  
   validate :acceptable_image
-  
   def acceptable_image
     return unless image.attached?
       acceptable_types = [ "image/gif", "image/jpeg", "image/png", "image/webp" ]
@@ -13,5 +11,5 @@ class Product < ApplicationRecord
       errors.add(:image, "must be a GIF, JPG, PNG or WEBP image")
     end
   end
-
+  validates :price, numericality: { greater_than_or_equal_to: 0.01 }
 end
