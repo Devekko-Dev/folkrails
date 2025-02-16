8fbb6ef73f2d8877a8cd0f2a6b7b5c323ee57d91
Nicholas Roberts <niccolox@devekko.com>
2025-02-16 12:46:06 -0800

    product validation

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,5 @@
 class Product < ApplicationRecord
+  validates :title, :subtitle, :description, :image, :price, presence: true
   has_one_attached :image
   after_commit -> { broadcast_refresh_later_to "products" }
 end
