d074b632ab92a2ea7aad84edd4eb7c1979f9d228
Nicholas Roberts <niccolox@devekko.com>
2025-02-16 20:57:32 -0800

    mailers

--- app/mailers/order_mailer.rb
+++ app/mailers/order_mailer.rb
@@ -15,9 +15,9 @@ class OrderMailer < ApplicationMailer
   #
   #   en.order_mailer.shipped.subject
   #
-  def shipped
-    @greeting = "Hi"
-
-    mail to: "to@example.org"
+  def shipped(order)
+    @order = order
+    mail to: order.email, subject: "Store Order Shipped"
   end
+
 end
